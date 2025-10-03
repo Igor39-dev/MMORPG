7387b4e9d21611f0efdbc2d6b7a8875a48e135f5
Igor39-dev <19gorokhov90@gmail.com>
2025-10-03 03:33:02 +0200

    added: views, signals.py, apps

---
+++ mmorpg_board/board/signals.py
@@ -0,0 +1,43 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
+
+from .models import Reply
+
+@receiver(post_save, sender=Reply)
+def notify_post_author_on_reply(sender, instance, created, **kwargs):
+    """Оповещает автора поста, если на его объявление оставили отклик"""
+    if created:
+        subject = "Новый отклик на ваше объявление"
+        message = (
+            f"Здравствуйте, {instance.post.author.username}!\n\n"
+            f"На ваше объявление «{instance.post.title}» оставлен отклик:\n\n"
+            f"{instance.text}\n\n"
+            f"Автор: {instance.author.username}"
+        )
+        send_mail(
+            subject,
+            message,
+            settings.DEFAULT_FROM_EMAIL,
+            [instance.post.author.email],
+            fail_silently=True,
+        )
+
+
+@receiver(post_save, sender=Reply)
+def notify_reply_author_on_accept(sender, instance, created, **kwargs):
+    """Оповещает автора отклика, если его отклик приняли"""
+    if not created and instance.is_accepted:
+        subject = "Ваш отклик принят!"
+        message = (
+            f"Здравствуйте, {instance.author.username}!\n\n"
+            f"Ваш отклик на объявление «{instance.post.title}» был принят."
+        )
+        send_mail(
+            subject,
+            message,
+            settings.DEFAULT_FROM_EMAIL,
+            [instance.author.email],
+            fail_silently=True,
+        )
--- mmorpg_board/board/views.py
+++ mmorpg_board/board/views.py
@@ -1,3 +1,107 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Post, Reply
+from .forms import PostForm
+
+class PostListView(ListView):
+    model = Post
+    template_name = "board/post_list.html"
+    context_object_name = "posts"
+    paginate_by = 10
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "board/post_detail.html"
+    context_object_name = "post"
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content", "category"]
+    template_name = "board/post_form.html"
+    success_url = reverse_lazy("post_list")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ["title", "content", "category", "is_active"]
+    template_name = "board/post_form.html"
+    success_url = reverse_lazy("post_list")
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            return redirect("post_list")
+        return super().dispatch(request, *args, **kwargs)
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = "board/post_confirm_delete.html"
+    success_url = reverse_lazy("post_list")
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            return redirect("post_list")
+        return super().dispatch(request, *args, **kwargs)
+
+
+class ReplyCreateView(LoginRequiredMixin, CreateView):
+    model = Reply
+    fields = ["text"]
+    template_name = "board/reply_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post_id = self.kwargs["pk"]
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy("post_detail", kwargs={"pk": self.kwargs["pk"]})
+
+
+class MyRepliesListView(LoginRequiredMixin, ListView):
+    model = Reply
+    template_name = "board/my_replies.html"
+    context_object_name = "replies"
+
+    def get_queryset(self):
+        return Reply.objects.filter(post__author=self.request.user)
+
+
+class ReplyAcceptView(LoginRequiredMixin, UpdateView):
+    model = Reply
+    fields = []
+    template_name = "board/reply_accept.html"
+    success_url = reverse_lazy("my_replies")
+
+    def form_valid(self, form):
+        form.instance.is_accepted = True
+        return super().form_valid(form)
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.post.author != self.request.user:
+            return redirect("my_replies")
+        return super().dispatch(request, *args, **kwargs)
+
+
+class ReplyDeleteView(LoginRequiredMixin, DeleteView):
+    model = Reply
+    template_name = "board/reply_confirm_delete.html"
+    success_url = reverse_lazy("my_replies")
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.post.author != self.request.user:
+            return redirect("my_replies")
+        return super().dispatch(request, *args, **kwargs)
