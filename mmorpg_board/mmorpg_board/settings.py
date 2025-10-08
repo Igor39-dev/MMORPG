61b4b7d0d9b12867e71a38081cdacaabe30e17ff
Igor39-dev <19gorokhov90@gmail.com>
2025-10-08 20:15:01 +0200

    .env, .gitingnore

--- mmorpg_board/mmorpg_board/settings.py
+++ mmorpg_board/mmorpg_board/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -164,7 +166,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'gorokhov-igor.g@yandex.ru'
-EMAIL_HOST_PASSWORD = 'dyfeuacckjjsfmxv'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
